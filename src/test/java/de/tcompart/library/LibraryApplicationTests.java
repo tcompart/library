235cd5fdbd66659982515ca33b1369caeb805792
Torsten Compart <torsten.compart@ionos.com>
2022-12-16 23:17:56 +0100

    Initial Spring Boot

---
+++ src/test/java/de/tcompart/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tcompart.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
